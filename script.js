fe01c092206cfa761dfa24da95eee630c999c641
flpvvvv <flpvvvv@live.com>
2022-04-17 17:16:43 +0000

    [ENH] Improve year calculation.

--- script.js
+++ script.js
@@ -1,47 +1,57 @@
-// var timer = null;
-// function displayClock(num) {
-//   //num是传入的startClock中的动态值
-//   if (num < 10) {
-//     return "0" + num;
-//   } else {
-//     return num;
-//   }
-// }
-// //停止计时
-// function stopClock() {
-//   clearTimeout(timer);
-// }
-//开始计时
+var bornTime = new Date("2022-03-30T00:33:00+02:00"); // Paris time
+
+// Start counting
 function startClock() {
-  const bornTime = new Date("2022-03-30T00:33:00+02:00");
   var currentTime = new Date();
-  // const currentTime = new Date("2022-03-30T00:33:00Z");
-  //显示时间
-  var deltaTime = currentTime - bornTime;
-  // var days = deltaTime / (1000 * 60 * 60 * 24);
-  show.innerHTML = forHumans(Math.round(deltaTime / 1000)); //在id为show的块区域显示
-  timer = setTimeout("startClock()", 1000); //延时器
+  var bornYear = bornTime.getFullYear();
+  var currentYear = currentTime.getFullYear();
+  var currentYearBirthday = new Date(bornTime.getTime());
+  currentYearBirthday.setFullYear(currentYear);
+  var lastYearBirthday = new Date(bornTime.getTime());
+  lastYearBirthday.setFullYear(currentYear - 1);
+
+  var deltaTime;
+  var age = currentYear - bornYear;
+  if (currentTime > currentYearBirthday) {
+    deltaTime = currentTime - currentYearBirthday;
+  } else {
+    deltaTime = currentTime - lastYearBirthday;
+    age = age - 1;
+  }
+
+  // console.log(age);
+
+  showDetail.innerHTML = forHumans(Math.round(deltaTime / 1000), age);
+
+  if (age > 0) {
+    showDays.innerHTML =
+      "(" +
+      toDays(Math.round((currentTime - bornTime) / 1000)).toLocaleString("fr") +
+      " days)";
+  }
+
+  // sleep for 1s
+  timer = setTimeout("startClock()", 1000);
   // console.log(currentTime.toISOString());
 }
-function forHumans(seconds) {
+function forHumans(seconds, age) {
+  var countDays = Math.floor(seconds / 86400);
   var levels = [
-    [Math.floor(seconds / 31536000), "years"],
-    [Math.floor((seconds % 31536000) / 86400), "days"],
-    [Math.floor(((seconds % 31536000) % 86400) / 3600), "hours"],
-    [Math.floor((((seconds % 31536000) % 86400) % 3600) / 60), "minutes"],
-    [(((seconds % 31536000) % 86400) % 3600) % 60, "seconds"]
+    [age, age === 1 ? "year" : "years"],
+    [countDays, countDays === 1 ? "day" : "days"],
+    [Math.floor((seconds % 86400) / 3600), "hr"],
+    [Math.floor(((seconds % 86400) % 3600) / 60), "min"],
+    [((seconds % 86400) % 3600) % 60, "sec"]
   ];
   var returntext = "";
 
   for (var i = 0, max = levels.length; i < max; i++) {
     if (levels[i][0] === 0) continue;
-    returntext +=
-      " " +
-      levels[i][0] +
-      " " +
-      (levels[i][0] === 1
-        ? levels[i][1].substr(0, levels[i][1].length - 1)
-        : levels[i][1]);
+    returntext += " " + levels[i][0] + " " + levels[i][1];
   }
   return returntext.trim();
+}
+
+function toDays(seconds) {
+  return Math.floor(seconds / 86400);
 }
